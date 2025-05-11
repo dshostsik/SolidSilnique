38bbaefee3e123ff7780eaa7fa2254129eb90d14
Daniil Shostsik 248680 <248680@edu.p.lodz.pl>
2025-05-11 02:40:01 +0200

    beginning implementation of the following system

---
+++ SolidSilnique/Core/ArtificialIntelligence/Follower.cs
@@ -0,0 +1,42 @@
+﻿#nullable enable
+
+namespace SolidSilnique.Core.ArtificialIntelligence;
+
+public class Follower
+{
+    private GameObject self;
+    private GameObject? target;
+    private float socialDistance;
+
+    public GameObject Self
+    {
+        get => self;
+    }
+    
+    public GameObject Target
+    {
+        get => target;
+        set => target = value;
+    }
+
+    public float SocialDistance
+    {
+        get => socialDistance;
+        set => socialDistance = value;
+    }
+
+    public Follower(GameObject self, float socialDistance = 200.0f)
+    {
+        this.self = self;
+    }
+
+    public void Follow()
+    {
+        if (target == null) return;
+        // if (self.transform.position < target.transform.position + socialDistance)
+        // {
+        //     self.transform.position += ;
+        // }
+    }
+
+}
