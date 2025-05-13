553b43506ff83971169689c031dd07b3bb54a363
Daniil Shostsik 248680 <248680@edu.p.lodz.pl>
2025-05-13 19:46:36 +0200

    changed the method name for a more understandable usage case

--- SolidSilnique/Core/ArtificialIntelligence/Follower.cs
+++ SolidSilnique/Core/ArtificialIntelligence/Follower.cs
@@ -38,7 +38,7 @@ namespace SolidSilnique.Core.ArtificialIntelligence
         public GameObject Self => _self;
 
         /// <summary>
-        /// Reference to a target <see cref="GameObject"/> that will be followed by <see cref="Self"/>.<p>Default value is <c>null</c>. Make sure that you set it before calling <see cref="Follow"/> method.</p>
+        /// Reference to a target <see cref="GameObject"/> that will be followed by <see cref="Self"/>.<p>Default value is <c>null</c>. Make sure that you set it before calling <see cref="GetFollowDirectionVector"/> method.</p>
         /// </summary>
         public GameObject Target
         {
@@ -88,7 +88,7 @@ namespace SolidSilnique.Core.ArtificialIntelligence
         }
 
         /// <summary>
-        /// Constructor.<p>Pay attention that <see cref="Target"/>'s default value is <c>null</c> and must be set before calling <see cref="Follow"/> method."/></p>
+        /// Constructor.<p>Pay attention that <see cref="Target"/>'s default value is <c>null</c> and must be set before calling <see cref="GetFollowDirectionVector"/> method."/></p>
         /// </summary>
         /// <param name="self">A self-object that should follow <see cref="Target"/></param>
         /// <param name="socialDistanceMultiplier">Multiplier for the distance between <see cref="Self"/> and <see cref="Target"/>. The default value is 1.5f</param>
@@ -103,7 +103,7 @@ namespace SolidSilnique.Core.ArtificialIntelligence
         /// <returns><c>direction</c> vector if <see cref="Target"/> is not <c>null</c>, else - <c>Vector3.Zero</c></returns>
         /// <exception cref="TargetNotSetException"> if <see cref="Target"/> is null</exception>
         /// </summary>
-        public Vector3 Follow()
+        public Vector3 GetFollowDirectionVector()
         {
             if (null == _target) throw new TargetNotSetException("Target is null.\nMaybe you forgot to set it?");
 
