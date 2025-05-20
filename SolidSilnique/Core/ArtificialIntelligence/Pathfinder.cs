605209a545b5760f34e24331933b18fff14550c5
Daniil Shostsik 248680 <248680@edu.p.lodz.pl>
2025-05-21 01:38:37 +0200

    continuation of pathfinder

--- SolidSilnique/Core/ArtificialIntelligence/Pathfinder.cs
+++ SolidSilnique/Core/ArtificialIntelligence/Pathfinder.cs
@@ -1,6 +1,7 @@
 ﻿#nullable enable
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using SolidSilnique.ProcderuralFoliage;
 
 namespace SolidSilnique.Core.ArtificialIntelligence
 {
@@ -14,30 +15,55 @@ namespace SolidSilnique.Core.ArtificialIntelligence
         /// </summary>
         private readonly GameObject _self;
 
-        // private readonly ProceduralGrass _terrain
-        
+        /// <summary>
+        /// Reference to self - object that should find the path to target
+        /// </summary>
+        public GameObject Self => _self;
+
+        /// <summary>
+        /// Generated map.
+        /// </summary>
+        private readonly ProceduralGrass _terrain;
+
         /// <summary>
         /// Collection of points that self should follow to reach the target
         /// </summary>
         private List<Vector3> _path;
-        
+
         /// <summary>
         /// Reference to target - object that self should reach
         /// </summary>
         private GameObject? _target;
 
         /// <summary>
+        /// Point the <see cref="Self"/> <see cref="GameObject"/> should reach
+        /// </summary>
+        private Vector3? _destination;
+
+        /// <summary>
+        /// Point the <see cref="Self"/> <see cref="GameObject"/> should reach
+        /// </summary>
+        public Vector3? Destination
+        {
+            get => _destination;
+            set => _destination = value;
+        }
+
+        /// <summary>
         /// Counted distance between self and target
         /// </summary>
         private float _distance;
+        
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="self">reference to self - object that should find the path to target</param>
-        public Pathfinder(GameObject self)
+        /// <param name="terrain">reference to the generated map</param>
+        public Pathfinder(GameObject self, ProceduralGrass terrain)
         {
             _self = self;
+            _terrain = terrain;
             _path = new();
             _distance = 0;
         }
